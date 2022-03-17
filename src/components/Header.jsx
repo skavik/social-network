3b76acd77c5ad07fb9efebdb26951d0bd4b81c88
skavik <tomniukj@gmail.com>
2022-03-17 22:17:22 +0200

    add header and navbar

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Header = () => {
+    return (
+        <header className='header'>
+        <img src='https://play-lh.googleusercontent.com/p6kS3dCcILt9Z4vRMxHXZTbRecqnZTx5ysBVp6Qe3fDofokRLuWjRxF8J0TkMTG2gKo' />
+      </header>
+    )
+};
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+
+const Profile = () => {
+    return (
+      <div className='content'>
+        <div>
+         <img src='https://q-xx.bstatic.com/xdata/images/hotel/840x460/78809294.jpg?k=cf850d507a9671cf7ff85d598435ea329a28cd4f1b1abc25c1892c91156d36ad&o='  />
+        </div>
+        <div>
+          ava + descripyion
+        </div>
+        <div>
+          My post
+          <div>
+            New post
+          </div>
+          <div>
+            post 1
+          </div>
+          <div>
+            post 2
+          </div>
+        </div>
+      </div>
+    )
+};
+
+export default Profile;
