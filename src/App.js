0ea5070f866f574ef6c101ce43577908a74ba175
skavik <tomniukj@gmail.com>
2022-03-22 21:50:17 +0200

    add component Dialogs

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
+import Dialogs from './components/Dialogs/Dialogs';
 
 
 function App() {
@@ -11,8 +12,10 @@ function App() {
     <div className="app-wrapper">
       <Header />
       <Navbar />
-      <Profile />
-      
+      <div className="content">
+        {/* <Profile /> */}
+        <Dialogs />
+      </div>
     </div>
   );
 };
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,7 +4,7 @@ import style from  './Profile.module.css'
 
 const Profile = () => {
     return (
-      <div className={style.content}>
+      <div>
         <div>
          <img src='https://q-xx.bstatic.com/xdata/images/hotel/840x460/78809294.jpg?k=cf850d507a9671cf7ff85d598435ea329a28cd4f1b1abc25c1892c91156d36ad&o='  />
         </div>
