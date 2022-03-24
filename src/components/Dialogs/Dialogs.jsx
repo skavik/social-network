66fc7700d72e59efaf2151bd21d6a81069b60b0c
skavik <tomniukj@gmail.com>
2022-03-24 21:55:06 +0200

    fix Dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,33 +1,40 @@
 import React from "react";
+import { BrowserRouter, NavLink, Route, Router } from "react-router-dom";
 import style from './Dialogs.module.css'
+
+const Dialog = (props) => {
+    return (
+        <div className={style.dialog}>
+            <NavLink to={"/dialogs/" + props.id}>{props.name}</NavLink>
+        </div>
+    )
+}
+
+
+const Message = (props) => {
+    return (
+        <div className={style.message}>
+            {props.message} 
+        </div>
+    )
+}
 
 const Dialogs = () => {
     return (
+        
         <div className={style.dialogs}>
             <div className={style.dialogsItem}>
-                <div className={style.dialog}>
-                    Adrii
-                </div>
-                <div className={style.dialog}>
-                    Slavik
-                </div>
-                <div className={style.dialog}>
-                    Anastasiia
-                </div>
-                <div className={style.dialog}>
-                    Valerii
-                </div>
+                <Router>
+                    <Route path="/dialogs/1" element={<Dialog name='Dima' id='1' />} />
+                    <Route path="/dialogs/2" element={<Dialog name='Slavik' id='2' />} />
+                    <Route path="/dialogs/3" element={<Dialog name='Anastasiia' id='3' />} />
+                    <Route path="/dialogs/4" element={<Dialog name='Valeriii' id='4' />} />            
+                </Router>
             </div>
            <div className={style.messages}>
-                <div className={style.message}>
-                    Hi 
-                </div>
-                <div>
-                    Hi what happens
-                </div>
-                <div>
-                    everything cool
-                </div>
+               <Message message='Hi' />
+               <Message message='Hi what happens' />
+               <Message message='everything cool' />
            </div>
        </div>
     )
