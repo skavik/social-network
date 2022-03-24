c01fab8cea668ba266d9061df981a5ad4174c1eb
skavik <tomniukj@gmail.com>
2022-03-24 16:26:05 +0200

    made Dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,32 @@ import style from './Dialogs.module.css'
 
 const Dialogs = () => {
     return (
-        <div>
-       Dialogs
+        <div className={style.dialogs}>
+            <div className={style.dialogsItem}>
+                <div className={style.dialog}>
+                    Adrii
+                </div>
+                <div className={style.dialog}>
+                    Slavik
+                </div>
+                <div className={style.dialog}>
+                    Anastasiia
+                </div>
+                <div className={style.dialog}>
+                    Valerii
+                </div>
+            </div>
+           <div className={style.messages}>
+                <div className={style.message}>
+                    Hi 
+                </div>
+                <div>
+                    Hi what happens
+                </div>
+                <div>
+                    everything cool
+                </div>
+           </div>
        </div>
     )
 };
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,6 +6,7 @@ const Header = () => {
     return (
         <header className={style.header}>
         <img src='https://play-lh.googleusercontent.com/p6kS3dCcILt9Z4vRMxHXZTbRecqnZTx5ysBVp6Qe3fDofokRLuWjRxF8J0TkMTG2gKo' />
+        <p>SOCIAL NETWORK</p>      
       </header>
     )
 };
