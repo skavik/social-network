4b54a44c8d3c30380eb744694e71d9e2a2fee4fd
skavik <tomniukj@gmail.com>
2022-04-22 15:34:55 +0300

    refactoring Users

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,9 +3,7 @@ import React from "react";
 import style from "./Users.module.css";
 
 class Users extends React.Component {
-  constructor(props) {
-    super(props);
-
+  componentDidMount() {
     axios
       .get("https://social-network.samuraijs.com/api/1.0/users")
       .then((response) => {
